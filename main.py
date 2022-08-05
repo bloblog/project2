940f101866acba2e687bba935267192005d92cf6
HYESEUNG <h103906@naver.com>
2022-08-05 16:04:56 +0900

    topics

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-'# Hello World'
+import streamlit as st
+st.write()
+
+topics = [
+    {"id":1, "title":"commit", "view":100},
+    {"id":2, "title":"push", "view":200},
+    {"id":3, "title":"pull", "view":50}
+]
+topics
